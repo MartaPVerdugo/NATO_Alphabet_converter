06ef250fbe46ce5c9ced89fa89f0acc727a6391b
marta.verdugo <marta.verdugo@nanoporetech.com>
2022-10-07 16:01:38 +0100

    tidy up

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import pandas
 
 alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
-
 alphabet_dataframe = pandas.DataFrame(alphabet)
-
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet_dataframe.iterrows()}
-# print(alphabet_dict)
-
-while True:
-    try:
-        user_word = input("Enter a word: ").upper()
-        user_list = [alphabet_dict[letter] for letter in user_word]
-    except KeyError:
-        print("Sorry, you must enter a letter of the alphabet.")
-    else:
-        print(user_list)
-        break
 
 
+def generate_nato():
+    while True:
+        try:
+            user_word = input("Enter a word: ").upper()
+            user_list = [alphabet_dict[letter] for letter in user_word]
+        except KeyError:
+            print("Sorry, you must enter a letter of the alphabet.")
+        else:
+            print(user_list)
+            break
+
+
+generate_nato()
