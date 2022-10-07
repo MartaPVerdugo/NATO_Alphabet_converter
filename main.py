43b02b94df8f8cfc67f7d62c1960eec70ecd128e
marta.verdugo <marta.verdugo@nanoporetech.com>
2022-10-07 13:06:10 +0100

    added exception for when entering a number instead of a letter

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dataframe = pandas.DataFrame(alphabet)
 
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet_dataframe.iterrows()}
-print(alphabet_dict)
+# print(alphabet_dict)
 
-user_word = input("Enter a word: ").upper()
+while True:
+    try:
+        user_word = input("Enter a word: ").upper()
+        user_list = [alphabet_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, you must enter a letter of the alphabet.")
+    else:
+        print(user_list)
+        break
 
-user_list = [alphabet_dict[letter] for letter in user_word if letter in alphabet_dict.keys()]
 
-print(user_list)
